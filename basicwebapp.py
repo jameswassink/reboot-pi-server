acac0c90433c8a69e019d14e77cb00ceb67d7094
James Wassink <james.wassink@gmail.com>
2018-07-28 15:31:28 +1000

    a couple of pages

--- basicwebapp.py
+++ basicwebapp.py
@@ -1,8 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template, request
 app = Flask(__name__)
 @app.route('/')
 def index():
-	return "Hello World"
+	return render_template("index.html")
+
+@app.route('/hello', methods=["POST"])
+def hello():	
+    return render_template('hello.html', name=request.form['helloName'])
+    
+@app.route('/bye')
+def bye():
+	return "Goodbye World"
 	
 if __name__ == '__main__':
 	app.run(debug=True, host='0.0.0.0')
+
