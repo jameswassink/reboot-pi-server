e455f59411766dff229566591586bd4af89837e1
James - RPi <james.wassink@gmail.com>
2018-09-22 14:51:14 +1000

    button

--- button.py
+++ button.py
@@ -1,9 +1,19 @@
 import RPi.GPIO as GPIO
+from time import sleep
 
-GPIO.setMode(GPIO.BCM)
+GPIO.setmode(GPIO.BCM)
 buttonPin = 8
+ledPin = 23
 GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(ledPin, GPIO.OUT)
 
-buttonVal = GPIO.input(buttonPin)
+def onBtnPress(pin):
+        print("button pressed")
+        GPIO.output(ledPin, 1)
+        sleep(0.5)
+        GPIO.output(ledPin, 0)
 
-print buttonVal
+GPIO.add_event_detect(buttonPin, GPIO.FALLING, callback=onBtnPress, bouncetime=200)
+
+message = input("Press enter to quit\n\n") # Run until someone presses enter
+GPIO.cleanup() # Clean up
